9bbb14596ec21a58ac2e238215b750fff236b530
rickiangela <rickiangela@gmail.com>
2019-04-09 21:17:24 -0400

    Updates

---
+++ precache-manifest.a0b81fb359c73c3e2d4ee7940c3faae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "6cd8fb43d60f5fb20a35",
+    "url": "/clickygame/static/js/main.f71a579b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6cd8fb43d60f5fb20a35",
+    "url": "/clickygame/static/css/main.9669d346.chunk.css"
+  },
+  {
+    "revision": "692e41d80e4a8d32b967b2f59b969a81",
+    "url": "/clickygame/index.html"
+  }
+];
